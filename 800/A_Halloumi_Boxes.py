0f58fede65bc03835550ce64ab05e36a8daeedcc
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2025-07-04 01:37:25 +0300

    found 3 answers

---
+++ 800/A_Halloumi_Boxes.py
@@ -0,0 +1,12 @@
+t = int(input())
+for _ in range(t):
+    n, k = list(map(int, input().split()))
+    arr = list(map(int, input().split()))
+ 
+    ss = sorted(list(arr))
+    if ss == arr:
+        print("YES")
+    elif k == 1:
+        print("NO")
+    else:
+        print("YES")
